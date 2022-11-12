ae82f2be9e14668685f102168587a831a032f3d2
Ahmed Hany <ahmed_hany19@outlook.com>
2022-11-12 16:21:35 +0200

    My customizations

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -1,10 +1,6 @@
-import ProductCard from '../product-card/product-card.component';
+import ProductCard from "../product-card/product-card.component";
 
-import {
-  CategoryPreviewContainer,
-  Title,
-  Preview,
-} from './category-preview.styles';
+import { CategoryPreviewContainer, Title, Preview } from "./category-preview.styles";
 
 const CategoryPreview = ({ title, products }) => {
   return (
