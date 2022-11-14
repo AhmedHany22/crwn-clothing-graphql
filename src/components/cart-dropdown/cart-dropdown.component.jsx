13551b9d7350418ffc0f58cdc705ba8de9b2c8d1
Ahmed Hany <ahmed_hany19@outlook.com>
2022-11-14 17:10:22 +0200

    Performance Optimizations : apply { useCallback, useMemo, memo, dynamic_import(suspense & lazy) }

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,24 +1,23 @@
-import { useContext } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useCallback, useContext } from "react";
+import { useNavigate } from "react-router-dom";
 
-import { CartContext } from '../../contexts/cart.context';
+import { CartContext } from "../../contexts/cart.context";
 
-import Button from '../button/button.component';
-import CartItem from '../cart-item/cart-item.component';
+import Button from "../button/button.component";
+import CartItem from "../cart-item/cart-item.component";
 
-import {
-  CartDropdownContainer,
-  EmptyMessage,
-  CartItems,
-} from './cart-dropdown.styles';
+import { CartDropdownContainer, EmptyMessage, CartItems } from "./cart-dropdown.styles";
 
 const CartDropdown = () => {
   const { cartItems } = useContext(CartContext);
   const navigate = useNavigate();
 
-  const goToCheckoutHandler = () => {
-    navigate('/checkout');
-  };
+  // {useCallback} will memoize the input func itself not the return in reverse to {useMemo}
+  // So it wont be initialized every time the comp rerenders
+  // It'll only rerender when the values in the 2nd input the array changes.
+  const goToCheckoutHandler = useCallback(() => {
+    navigate("/checkout");
+  }, []);
 
   return (
     <CartDropdownContainer>
